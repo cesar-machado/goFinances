26b0e6a6d0012399e4fe5eaa1aa5e5cd2090f88b
cesar-machado <cesarmach19@gmail.com>
2022-01-12 16:25:32 -0300

    Highlight card pronto

---
+++ src/components/HighlightCard/styles.ts
@@ -0,0 +1,71 @@
+import styled, { css } from 'styled-components/native';
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+
+interface TypeProps {
+  type: 'up' | 'down' | 'total';
+}
+
+export const Container = styled.View<TypeProps>`
+  background-color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.secondary : theme.colors.shapes};
+
+  width: ${RFValue(300)}px;
+  border-radius: 5px;
+
+  padding: 19px 23px;
+  padding-bottom: ${RFValue(42)}px;
+  margin-right: 16px;
+`;
+
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text<TypeProps>`
+  font-family: ${({ theme }) => theme.Fonts.medium};
+  font-size: ${RFValue(14)}px;
+
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shapes : theme.colors.text_dark};
+`;
+
+export const Icon = styled(Feather)<TypeProps>`
+  font-size: ${RFValue(40)}px;
+  ${({ type }) =>
+    type === 'up' &&
+    css`
+      color: ${({ theme }) => theme.colors.success};
+    `}
+
+  ${({ type }) =>
+    type === 'down' &&
+    css`
+      color: ${({ theme }) => theme.colors.attention};
+    `}
+
+  ${({ type }) =>
+    type === 'total' &&
+    css`
+      color: ${({ theme }) => theme.colors.shapes};
+    `}
+`;
+
+export const Footer = styled.View``;
+
+export const Amount = styled.Text<TypeProps>`
+  font-family: ${({ theme }) => theme.Fonts.medium};
+  font-size: ${RFValue(32)}px;
+
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shapes : theme.colors.text_dark};
+  margin-top: 38px;
+`;
+
+export const LastTransaction = styled.Text<TypeProps>`
+  font-family: ${({ theme }) => theme.Fonts.regular};
+  font-size: ${RFValue(12)}px;
+  color: ${({ theme, type }) =>
+    type === 'total' ? theme.colors.shapes : theme.colors.text}; ;
+`;
